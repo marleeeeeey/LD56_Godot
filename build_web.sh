c5b604f94e3c2edfe8f215d30a9d6cb8b630df54
Konstantin Poleshchuk <javascriptoverlord@gmail.com>
2024-10-05 01:00:34 +0400

    added publish script

--- build_web.sh
+++ build_web.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+# Define variables
+EXPORT_NAME="web"
+RELEASE_FOLDER="./releases"
+EXPORT_PATH="${RELEASE_FOLDER}/web"
+EXPORT_ZIP="${EXPORT_NAME}.zip"
+
 clean() {
     echo "Cleaning web export directory..."
-    rm -rf ./releases/web/*
-    rm -rf ./releases/godot_web_export.tar.gz
-    mkdir ./releases/web
+    rm -rf "${EXPORT_PATH}"/*
+    rm -rf "${RELEASE_FOLDER}/${EXPORT_ZIP}"
+    mkdir -p "${EXPORT_PATH}"
 }
 
 build() {
     echo "Building Godot project for web export..."
-    godot --headless --export-debug "Web" ./releases/web/index.html
+    godot --headless --export-debug "Web" "${EXPORT_PATH}/index.html"
 }
 
 compress() {
     echo "Compressing web export files..."
-    cd ./releases
-    tar -czvf godot_web_export.tar.gz web
+    cd "${RELEASE_FOLDER}"
+    zip -r "${EXPORT_ZIP}" web
     cd ..
-    rm -rf ./releases/web/
+    rm -rf "${EXPORT_PATH}"
 }
 
 echo "Starting Godot web export process..."
@@ -26,4 +32,4 @@ build
 compress
 
 echo "Godot web export build process completed!"
-echo "Your web export is now available in ./releases/godot_web_export.tar.gz"
+echo "Your web export is now available in ${RELEASE_FOLDER}/${EXPORT_ZIP}"
