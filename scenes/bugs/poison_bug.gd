5e1f89aabd595d729d0530e55da7e64723920a12
Konstantin Poleshchuk <javascriptoverlord@gmail.com>
2024-10-07 02:13:48 +0400

    added posion cloude

--- scenes/bugs/poison_bug.gd
+++ scenes/bugs/poison_bug.gd
@@ -2,7 +2,8 @@ extends BaseBug
 
 class_name PoisonBug
 
-const poison_cloud_scene: PackedScene = preload("res://scenes/cloud/cloud.tscn")
+const poison_cloud_texture: Texture = preload("res://assets/images/cloudet.png")
+const trails_scene: PackedScene = preload("res://scenes/trail/trail.tscn")
 
 func process_additional_logic(_delta: float):
 	pass
@@ -11,7 +12,10 @@ func update_additional_state(_delta: float):
 	pass
 
 func use_super_power():
-	var cloud = poison_cloud_scene.instantiate()
-	cloud.position = position
-	GameManager.spawn_scene(cloud)
+	var trail: Trail = trails_scene.instantiate()
+	trail.position = position
+	trail.texture = poison_cloud_texture
+	trail.z_index = 10
+	trail.max_life_time = 5
+	GameManager.spawn_scene(trail)
 	print("PoisonBug: use_super_power")
--- scenes/trail/trail.gd
+++ scenes/trail/trail.gd
@@ -5,6 +5,7 @@ class_name Trail
 @export var texture: Texture2D
 @export var max_life_time := 3.0
 @export var opacity_multiplier := 1
+@export var has_fade_out := true
 
 @onready var sprite: Sprite2D = $Sprite2D
 
@@ -17,13 +18,16 @@ func _ready() -> void:
 	$Timer.start()
 
 func _on_timer_timeout() -> void:
-	life_time += 1
+	life_time += update_time
 	update_opacity()
 
 	if life_time >= max_life_time:
 		queue_free()
 
 func update_opacity():
+	if !has_fade_out:
+		return
+
 	var progress = life_time / max_life_time
 	var opacity = 1.0 - (progress * opacity_multiplier)
 	sprite.modulate.a = opacity
