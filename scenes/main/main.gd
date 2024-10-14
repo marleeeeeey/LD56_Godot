83771d9d60f5412158c2fcc1387b4efc0a227625
marleeeeeey <marleeeeeey@gmail.com>
2024-10-14 17:11:58 +0400

    Refactoring.

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,14 +1,13 @@
 extends Node2D
 
 var enemy_spawn_timer: Timer
-var enemy_scene: PackedScene = preload("res://scenes/enemy/enemy.tscn")
-var child_scene: PackedScene = preload("res://scenes/child/child.tscn")
+const enemy_scene: PackedScene = preload("res://scenes/enemy/enemy.tscn")
+const child_scene: PackedScene = preload("res://scenes/child/child.tscn")
 
 
 func _ready() -> void:
 	enemy_spawn_timer = Globals.create_timer(self, 0, on_emeny_spawn_timeout)
 
-	# create several children
 	for i in range(5):
 		var pos = Globals.get_random_point_in_area($ChildSpawnArea2D)
 		var child: Child = Globals.create_on_parent(self, pos, child_scene)
@@ -20,4 +19,4 @@ func on_emeny_spawn_timeout() -> void:
 	var enemy: Enemy = Globals.create_in_global_pos(self, pos, enemy_scene)
 	enemy.set_base_area($EnemySpawnArea2D)
 	enemy.set_dead_area($EnemyDeadArea2D)
-	enemy_spawn_timer.start(Globals.get_random_near_value(1000))
+	enemy_spawn_timer.start(Globals.get_random_near_value(3))
